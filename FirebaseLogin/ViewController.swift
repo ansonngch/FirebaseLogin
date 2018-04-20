3525cb82c856820ae8863c43db63c2276b54512d
Anson Ng <ansonng05121994@gmail.com>
2018-04-20 21:28:06 +0800

    Initial Commit for FirebaseLogin app
    FirebaseLogin app is an app that consist of a signup page and a login page which uses firebase to run.

---
+++ FirebaseLogin/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirebaseLogin
+//
+//  Created by Anson Ng on 20/4/18.
+//  Copyright © 2018 Anson Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
